983b44ca2b5a11a54cbd8ae6aa1af43ca00a2e95
wazzah-marham <wazzah@marham.pk>
2022-10-31 16:48:11 +0500

    changes log to toast

--- marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/MarhamVideoCallHelper.java
+++ marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/MarhamVideoCallHelper.java
@@ -1,11 +1,12 @@
 package com.marham.marhamvideocalllibrary;
 
-import android.util.Log;
+import android.content.Context;
+import android.widget.Toast;
 
 public class MarhamVideoCallHelper {
 
-    public static void initHelper() {
-        Log.e("Wazzah", "MarhamVideoCallHelper initialized");
+    public static void initHelper(Context context){
+        Toast.makeText(context,"MarhamVideoCallHelper Initialized",Toast.LENGTH_SHORT).show();
     }
 
 }
