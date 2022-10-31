beaa08621ecde2b90eafc90bc0e31400480cfada
wazzah-marham <wazzah@marham.pk>
2022-10-31 16:09:16 +0500

    first commit

---
+++ marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/MarhamVideoCallHelper.java
@@ -0,0 +1,11 @@
+package com.marham.marhamvideocalllibrary;
+
+import android.util.Log;
+
+public class MarhamVideoCallHelper {
+
+    public static void initHelper() {
+        Log.e("Wazzah", "MarhamVideoCallHelper initialized");
+    }
+
+}
