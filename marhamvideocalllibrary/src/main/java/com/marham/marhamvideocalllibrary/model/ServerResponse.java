5b0e93ba568099f4c0537aa4fd12cd3ab4c8937b
wazzah-marham <wazzah@marham.pk>
2022-12-05 12:53:43 +0500

    code after setting up network layer

---
+++ marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/model/ServerResponse.java
@@ -0,0 +1,48 @@
+package com.marham.marhamvideocalllibrary.model;
+
+import java.util.List;
+
+import okhttp3.internal.http2.Header;
+
+public class ServerResponse {
+
+
+    private List<Header> headerList;
+    private int requestCode;
+    private String return_status;
+    private String message;
+
+    public void setHeaderList(List<Header> headerList) {
+        this.headerList = headerList;
+    }
+
+    public void setRequestCode(int code) {
+        this.requestCode = code;
+    }
+
+    public void setReturn_status(String return_status) {
+        this.return_status = return_status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Header> getHeaderList() {
+        return headerList;
+    }
+
+    public int getRequestCode() {
+        return requestCode;
+    }
+
+
+    public String getReturn_status() {
+        return return_status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
--- marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/network/MarhamVideoCallEndPoints.java
+++ marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/network/MarhamVideoCallEndPoints.java
@@ -1,4 +1,17 @@
 package com.marham.marhamvideocalllibrary.network;
 
-public class MarhamVideoCallEndPoints {
+import com.marham.marhamvideocalllibrary.model.ServerResponse;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface MarhamVideoCallEndPoints {
+    @FormUrlEncoded
+    @POST("index.php?action=login")
+    Call<ServerResponse> login(@FieldMap HashMap<String, String> info);
+
 }
