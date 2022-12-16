8284a1a83d84232118d8ce27766d96635e86847f
wazzah-marham <wazzah@marham.pk>
2022-12-16 13:11:57 +0500

    code after adding header of dr profile

---
+++ marhamvideocalllibrary/src/main/java/com/marham/marhamvideocalllibrary/activities/doctor/DoctorProfileActivity.java
@@ -0,0 +1,15 @@
+package com.marham.marhamvideocalllibrary.activities.doctor;
+
+import android.os.Bundle;
+
+import com.marham.marhamvideocalllibrary.R;
+import com.marham.marhamvideocalllibrary.activities.BaseActivity;
+
+public class DoctorProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+    }
+}
